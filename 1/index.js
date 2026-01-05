ebad50a66eb4cb083e99aacbb65ad93845262bb8
Eric Basilio <micoplayer3@gmail.com>
2026-01-05 19:38:42 -0300

    aula 1

---
+++ 1/index.js
@@ -0,0 +1,5 @@
+console.log(`Hello World`);
+window.alert(`ALERTA`);
+
+document.getElementById('Header').textContent = `Escrito usando JS`;
+document.getElementById('Paragraph').textContent = `mesma coisa`;
